6a1ce41aac73405cb0f70c49ef3c0b88ecee4006
2417 <w2014@e-mirim.hs.kr>
2021-04-11 20:39:04 +0900

    list1 바꾸기 성공(index 접근)

---
+++ 연습.py
@@ -0,0 +1,12 @@
+list1 = [10, 20, 30, 40, 50]
+for a in list1:
+    a += 1  # 이렇게 하면 안됌!!
+
+print(list1)
+
+
+# list1 = [10, 20, 30, 40, 50]  #배열 1씩 추가
+# for i in range(0, len(list1)):
+#     list1[i] += 1
+#
+# print(list1)
