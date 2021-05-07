67bd1b4bff2eb03fa304c7a6d7d74ef8af0f7d5b
2417 <w2014@e-mirim.hs.kr>
2021-05-07 12:07:18 +0900

    오늘 배열 배우기

--- 연습.py
+++ 연습.py
@@ -10,3 +10,24 @@ print(list1)
 #     list1[i] += 1
 #
 # print(list1)
+
+#--------------------------------------
+
+arr = [1,2, 3, 3, 1, 3, 3, 2, 3, 2]
+count = [0, 0, 0, 0]
+
+for x in arr:
+    if x == 1:
+        count[1] += 1
+    elif x == 2:
+        count[2] += 1
+    elif x == 3:
+        count[3] += 1
+
+count[1] = 일
+count[2] = 이
+count[3] = 삼
+
+print(f"1의 갯수 : {count[1]}")
+print(f"2의 갯수 : {count[2]}")
+print(f"3의 갯수 : {count[3]}")
