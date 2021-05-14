098664fcc0c0df65c21abed2f292d74e0a50ed88
2417 <w2014@e-mirim.hs.kr>
2021-05-14 09:37:38 +0900

    문제4번의 정답

--- 수행 문제.py
+++ 수행 문제.py
@@ -12,15 +12,15 @@
 
 def solution(arr):
     left = 0
-    right = len(arr)
+    right = len(arr) - 1
 
-    while @@@:
+    while (left < len(arr)/2):
         #arr[left]과 arr[right]의 값을 서로 바꿈
         # temp = arr[left]
         # arr[left] = arr[right]
         # arr[right] = temp
 
-        arr[left], arr[right] = arr[right], arr[left]
+        arr[left], arr[right] = arr[right], arr[left]   #위의 코드와 같음
 
         left += 1
         right -= 1
@@ -28,3 +28,5 @@ def solution(arr):
 
 arr = [1, 4, 2, 3]
 ret = solution(arr)
+
+print("Solution: return value of the function is ", ret, " .")
