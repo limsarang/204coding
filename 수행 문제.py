2529e72b1bf1005ac98542355d1766570fdfa1c2
2417 <w2014@e-mirim.hs.kr>
2021-05-14 09:08:30 +0900

    문제4번 python 스타일의 swap

--- 수행 문제.py
+++ 수행 문제.py
@@ -16,9 +16,11 @@ def solution(arr):
 
     while @@@:
         #arr[left]과 arr[right]의 값을 서로 바꿈
-        temp = arr[left]
-        arr[left] = arr[right]
-        arr[right] = temp
+        # temp = arr[left]
+        # arr[left] = arr[right]
+        # arr[right] = temp
+
+        arr[left], arr[right] = arr[right], arr[left]
 
         left += 1
         right -= 1
