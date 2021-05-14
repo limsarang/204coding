59e4a12a2a8c68eb0ceb11b17e49fd05e1afda99
2417 <w2014@e-mirim.hs.kr>
2021-05-14 10:02:10 +0900

    문제6번 정답

--- 수행 문제.py
+++ 수행 문제.py
@@ -36,7 +36,7 @@ print("Solution: return value of the function is ", ret, " .")
 def solution(scores):
    count = 0
    for s in scores:
-       if 650 <= scores[s] or scores[s] < 800:
+       if 650 <= scores[s] and scores[s] < 800:
            count += 1
    return count
 
