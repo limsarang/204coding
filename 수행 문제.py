4370280712f621858b215c71305799cc6d9498e5
2417 <w2014@e-mirim.hs.kr>
2021-05-14 09:59:53 +0900

    문제6번

--- 수행 문제.py
+++ 수행 문제.py
@@ -9,7 +9,7 @@
 # 최대값(arr)
 
 #----------------------------------
-
+#4
 def solution(arr):
     left = 0
     right = len(arr) - 1
@@ -30,3 +30,19 @@ arr = [1, 4, 2, 3]
 ret = solution(arr)
 
 print("Solution: return value of the function is ", ret, " .")
+
+#----------------------------------
+#6
+def solution(scores):
+   count = 0
+   for s in scores:
+       if 650 <= scores[s] or scores[s] < 800:
+           count += 1
+   return count
+
+#The following is code to output testcase. The code below is correct and you shall correct solution function.
+scores = [650, 722, 914, 558, 714, 803, 650, 679, 669, 800]
+ret = solution(scores)
+
+#Press Run button to receive output.
+print("Solution: return value of the function is", ret, ".")
