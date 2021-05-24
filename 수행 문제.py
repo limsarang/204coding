932322a858968f7bdd526546498f1f8f1f291eaf
2417 <w2014@e-mirim.hs.kr>
2021-05-24 16:23:54 +0900

    홈셰프 앱 콘솔 프로그램으로 만들기

--- 수행 문제.py
+++ 수행 문제.py
@@ -87,3 +87,33 @@ ret2 = solution(sentence2)
 
 # Press Run button to receive output.
 print("Solution: return value of the function is", ret2, ".")
+
+#----------------------------------
+#8
+
+# arr = 'abcdefg'
+#
+# # print(arr[len(arr)]) #에러
+# print(arr[3])          #d
+# print(arr[6])          #g
+# print(arr[0])          #a
+# print(arr[-1])         #g
+
+
+def solution(characters):
+   result = ""
+   result += characters[0]
+   for i in range(1, len(characters)):
+       if characters[i - 1] != characters[i]:
+           result += characters[i]
+   return result
+
+#The following is code to output testcase. The code below is correct and you shall correct solution function.
+characters = "senteeeencccccceeee"
+ret = solution(characters)
+
+#Press Run button to receive output.
+print("Solution: return value of the function is { ", ret, " }")
+
+#----------------------------------
+#10
