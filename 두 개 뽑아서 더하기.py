5d008472949a3ba4c78d2427069c39aa03e28a00
2417 <w2014@e-mirim.hs.kr>
2021-06-18 09:21:17 +0900

    두 개 뽑아서 더하기 [끝]

--- 두 개 뽑아서 더하기.py
+++ 두 개 뽑아서 더하기.py
@@ -2,15 +2,26 @@
 # numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서
 # 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return 하도록
 # solution 함수를 완성해주세요.
+from pip._vendor.pep517.check import ansi
+
 
 def main():
     numbers = [5, 0, 2, 7]
-
+    print()
 
 def solution(numbers):
-    answer = []
+    #answer는 비어있는 list(배열)
+    answer = list()
+    # 인덱스 접근
+    for i in range(len(numbers)-1):
+        for j in range(i+1, len(numbers)):
+            #numbers[i]는 첫 번째 선택한 숫자
+            #numbers[j]는 두 번째 선택한 숫자
+            if (numbers[i] + numbers[j]) not in answer:
+                #list 끝 부분에 값을 삽입
+                answer.append(numbers[i] + numbers[j])
+    answer.sort()
     return answer
-
 
 if __name__ == "main":
     main()
